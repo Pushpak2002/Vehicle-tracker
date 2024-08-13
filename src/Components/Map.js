7a2da165e97fbe1e0ff9d3cab3db40d75a988543
Pushpak2002 <kamblepushpak27@gmail.com>
2024-08-13 15:43:55 +0530

    Showing car info

--- src/Components/Map.js
+++ src/Components/Map.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { MapContainer, TileLayer, Marker, Polyline } from 'react-leaflet';
+import { MapContainer, TileLayer, Marker, Polyline, Tooltip } from 'react-leaflet'; // Import Tooltip
 import L from 'leaflet';
 import 'leaflet/dist/leaflet.css';
 
@@ -13,6 +13,11 @@ const carIcon = new L.Icon({
 });
 
 const Map = ({ vehiclePosition, path }) => {
+  const getCurrentTime = () => {
+    const now = new Date();
+    return now.toLocaleTimeString();
+  };
+
   return (
     <MapContainer center={[vehiclePosition.lat, vehiclePosition.lng]} zoom={13} style={{ height: "100vh", width: "100%" }}>
       <TileLayer
@@ -20,7 +25,26 @@ const Map = ({ vehiclePosition, path }) => {
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
       />
       {vehiclePosition && (
-        <Marker position={[vehiclePosition.lat, vehiclePosition.lng]} icon={carIcon} />
+        <Marker 
+          position={[vehiclePosition.lat, vehiclePosition.lng]} 
+          icon={carIcon}
+          eventHandlers={{
+            mouseover: (e) => {
+              e.target.openTooltip();
+            },
+            mouseout: (e) => {
+              e.target.closeTooltip();
+            },
+          }}
+        >
+          <Tooltip>
+            <div>
+              <h4>Car Information</h4>
+              <p>Location: {`Lat: ${vehiclePosition.lat}, Lng: ${vehiclePosition.lng}`}</p>
+              <p>Current Time: {getCurrentTime()}</p>
+            </div>
+          </Tooltip>
+        </Marker>
       )}
       <Polyline positions={path} color="red" />
     </MapContainer>
